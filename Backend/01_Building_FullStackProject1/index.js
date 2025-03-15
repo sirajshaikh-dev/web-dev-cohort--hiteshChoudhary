6ac763456b26aebcfeba9ebc3c6dd5400813abc3
sirajshaikh-dev <ss1425306@gmail.com>
2025-03-15 16:58:35 +0530

    initialize backend project with Express, dotenv, and CORS setup lecture upto 1:50:00hr

---
+++ Backend/01_Building_FullStackProject1/index.js
@@ -0,0 +1,31 @@
+import express from 'express'  
+import dotenv from 'dotenv'    
+import cors from 'cors'
+
+dotenv.config()  // Load environment variables from a .env file into process.env
+
+const app = express()
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET, POST, PUT, DELETE'],
+  allowedHeaders: ['Content-Type','Authorization']
+}))
+
+app.use(express.json())  // backend will accept json data
+const port = process.env.PORT || 3000 
+
+app.use(express.urlencoded({ extended: true }))  // backend will accept form data
+
+app.get('/', (req, res) => {   // req, res are objects that express passes to the callback function
+  res.send('Hello World!')
+})
+
+app.get('/siraj', (req, res) => { // this func will run when client send rqst to /siraj 
+  res.send('Hello Siraj!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
