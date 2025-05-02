939f5766d2b64068e5e627bab3afed7bea841598
sirajshaikh-dev <ss1425306@gmail.com>
2025-05-02 22:03:47 +0530

    add user registration functionality with user model and routes

--- Backend/01_Building_FullStackProject1/index.js
+++ Backend/01_Building_FullStackProject1/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'    
 import cors from 'cors'
 import db from './utils/db.js' //remember to add .js
+import userRoutes from './routes/user.routes.js' //remember to add .js
 
 dotenv.config()  // Load environment variables from a .env file into process.env
 
@@ -29,6 +30,9 @@ app.get('/siraj', (req, res) => { // this func will run when client send rqst to
 
 //connect to db
 db();
+
+//user routes
+app.use('/api/v1/users', userRoutes) // api/v1/user is the base url, uske baad jo bhi url hoga wo userRoutes.js se aayega
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
